9aad89994f9e59d7a6919f21563054731dbe7817
moobchunks <moobchunks@moobware.com>
2017-07-22 15:31:35 -0500

    Lean force kinda working... a little?

---
+++ Assets/Programming/DebugMobileInput.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class DebugMobileInput : MonoBehaviour {
+
+    public float secondsToWaitForTiltInput = .5f;
+
+    private float _startForwardTilt = 0f;
+    private bool _tiltIsCalibrated = false;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(CalibrateTiltInput(secondsToWaitForTiltInput));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (_tiltIsCalibrated)
+        {
+            Debug.Log("Vertical Axis: " + CrossPlatformInputManager.GetAxis("Vertical"));
+            var updatedTransformPosition = new Vector3 { x = transform.position.x, y = transform.position.y, z = transform.position.z };
+            updatedTransformPosition.x += CrossPlatformInputManager.GetAxis("Horizontal");
+            updatedTransformPosition.z -= (CrossPlatformInputManager.GetAxis("Vertical") - _startForwardTilt);
+
+            transform.position = updatedTransformPosition;
+        }
+    }
+
+    IEnumerator CalibrateTiltInput(float secondsToWait)
+    {
+        yield return new WaitForSeconds(secondsToWait);
+        _startForwardTilt = CrossPlatformInputManager.GetAxis("Vertical");
+        Debug.Log("Start Forward Tilt" + _startForwardTilt);
+        _tiltIsCalibrated = true;
+    }
+}
---
+++ Assets/Programming/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+    public GameObject gameObjectToApplyLeanForceTo;
+    public float leftToRightLeanForceMultiplier = 1f;
+    public float backToForwardLeanForceMultiplier = 1f;
+    public float constantUpwardForce = 1f;
+    public float secondsToWaitForTiltInput = .5f;
+
+    private Rigidbody _rigidbody;
+
+    private float _startForwardTilt = 0f;
+    private bool _tiltIsCalibrated = false;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(CalibrateTiltInput(secondsToWaitForTiltInput));
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate () {
+        if (_tiltIsCalibrated)
+        {
+            var leftToRightLean = CrossPlatformInputManager.GetAxis("Horizontal");
+            var backToForwardLean = (CrossPlatformInputManager.GetAxis("Vertical") - _startForwardTilt);
+
+            var leanForce = new Vector3(leftToRightLean * leftToRightLeanForceMultiplier, constantUpwardForce, backToForwardLean * backToForwardLeanForceMultiplier);
+            _rigidbody.AddForceAtPosition(leanForce, gameObjectToApplyLeanForceTo.transform.position);
+        } else
+        {
+            var leanForce = new Vector3(0, constantUpwardForce, 0);
+            _rigidbody.AddForceAtPosition(leanForce, gameObjectToApplyLeanForceTo.transform.position);
+        }
+    }
+
+    IEnumerator CalibrateTiltInput(float secondsToWait)
+    {
+        yield return new WaitForSeconds(secondsToWait);
+        _startForwardTilt = CrossPlatformInputManager.GetAxis("Vertical");
+        Debug.Log("Start Forward Tilt" + _startForwardTilt);
+        _tiltIsCalibrated = true;
+    }
+}
